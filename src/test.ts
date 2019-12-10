f9de6cba49146914e430ebc29114f7bb862acf7a
Carlos Alba <soradev4@gmail.com>
2019-12-10 10:45:56 -0800

    changes

--- src/test.ts
+++ src/test.ts
@@ -5,8 +5,8 @@ const { DataCrawler } = require('./index');
     const GetAlligator = await DataCrawler("https://alligator.io/js/filter-array-method/");
     const Objects = await GetAlligator.gatherHTML();
     Objects
-        .filter((e: { textContent: { includes: (arg0: string) => void; }; }) => e.textContent.includes('Filter'))
-        .filter((e: { tagName: string; }) => e.tagName === "DIV")
-        .filter((e: { attributes: { get: (arg0: string) => string; }; }) => e.attributes.get('class') === 'article-content')
+        .filter((e: any) => e.textContent.includes('Filter'))
+        .filter((e: any) => e.tagName === "DIV")
+        .filter((e: any) => e.attributes.get('class') === 'article-content')
         .forEach((e: any) => console.log(e));
 })();
